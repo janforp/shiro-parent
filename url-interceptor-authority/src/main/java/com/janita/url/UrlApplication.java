058348bb4b3e00a26c7c25e8ceff6b78948faf8f
janita <zcj880902>
2017-06-19 18:05:18 +0800

    基于 url 的权限管理

---
+++ url-interceptor-authority/src/main/java/com/janita/url/UrlApplication.java
@@ -0,0 +1,15 @@
+package com.janita.url;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Janita on 2017/6/19 0019-下午 5:44
+ * 该类是：基于 url 的权限管理
+ */
+@SpringBootApplication
+public class UrlApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UrlApplication.class, args);
+    }
+}
