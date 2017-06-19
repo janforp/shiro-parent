acbf4497fc92ea09b893cd5330f21c8a0620b202
janita <zcj880902>
2017-06-19 19:50:55 +0800

    基于 url 的权限管理

---
+++ url-interceptor-authority/src/main/java/com/janita/url/controller/LoginController.java
@@ -0,0 +1,11 @@
+package com.janita.url.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by Janita on 2017/6/19 0019-下午 7:48
+ * 该类是：用于登录
+ */
+@Controller
+public class LoginController {
+}
