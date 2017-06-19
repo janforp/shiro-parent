a34c13974c27799705d3e2efe82bb562bdd73c7e
janita <zcj880902>
2017-06-19 16:15:12 +0800

    视频day01

---
+++ shiro-day-one/src/main/java/com/janita/one/OneApplication.java
@@ -0,0 +1,16 @@
+package com.janita.one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Janita on 2017/6/19 0019-下午 3:25
+ * 该类是：
+ */
+@SpringBootApplication
+public class OneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneApplication.class, args);
+    }
+}
