6011445f23a3afe3e7f3030a5c6874f9bccbc0db
janita <zcj880902>
2017-06-20 17:30:27 +0800

    shiro-two

---
+++ shiro-two/src/main/java/com/janita/shiro/ShiroTwoApplication.java
@@ -0,0 +1,16 @@
+package com.janita.shiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Janita on 2017/6/20 0020-下午 5:26
+ * 该类是：
+ */
+@SpringBootApplication
+public class ShiroTwoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShiroTwoApplication.class, args);
+    }
+}
